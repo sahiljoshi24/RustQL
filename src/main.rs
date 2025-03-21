ea1a078d6ec3fa5113ab7811784cc572e02f56a8
sahiljoshi24 <jossahil10@gmail.com>
2025-03-21 19:45:31 +0530

    command-line interface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+mod db;
+mod query;
+
+use db::Database;
+use query::execute_sql;
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let mut db = Database::new();
+    loop {
+        print!("SQL> ");
+        io::stdout().flush().unwrap();
+
+        let mut query = String::new();
+        io::stdin().read_line(&mut query).unwrap();
+        let query = query.trim();
+
+        if query.eq_ignore_ascii_case("exit") {
+            break;
+        }
+
+        match execute_sql(&mut db, query) {
+            Ok(result) => println!("{}", result),
+            Err(err) => eprintln!("Error: {}", err),
+        }
+    }
 }
